85f7d402c6ab5c56c7630d4ffb878092a2584991
Pham Thanh Son <sonpt28@fpt.edu.vn>
2024-03-25 11:13:16 +0700

    CRUD Category

--- BookShop/Controllers/CategoryController.cs
+++ BookShop/Controllers/CategoryController.cs
@@ -32,9 +32,57 @@ namespace BookShop.Controllers
             {
                 _context.Categories.Add(category);
                 _context.SaveChanges();
+                TempData["success"] = "Category created succesfully";
                 return RedirectToAction("Index");
             }
 			return View();
+		}
+        public IActionResult Edit(int? id)
+        {
+            if (id==null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? category=_context.Categories.Find(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+		[HttpPost]
+		public IActionResult Edit(Category category)
+		{
+			if (ModelState.IsValid)
+			{
+				_context.Categories.Update(category);
+				_context.SaveChanges();
+				TempData["success"] = "Category updated succesfully";
+				return RedirectToAction("Index");
+			}
+			return View();
+		}
+		public IActionResult Delete(int? id)
+		{
+			if (id == null || id == 0)
+			{
+				return NotFound();
+			}
+			Category? category = _context.Categories.Find(id);
+			if (category == null)
+			{
+				return NotFound();
+			}
+			return View(category);
+		}
+		[HttpPost]
+		public IActionResult Delete(Category category)
+		{
+				_context.Categories.Remove(category);
+				_context.SaveChanges();
+				TempData["success"] = "Category deleted succesfully";
+				return RedirectToAction("Index");
+
 		}
 	}
 }
--- BookShop/Models/Category.cs
+++ BookShop/Models/Category.cs
@@ -8,6 +8,7 @@ namespace BookShop.Models
     {
         [Key]
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
         public string? Description { get; set; }
         [DisplayName("Display Order of Category")]
