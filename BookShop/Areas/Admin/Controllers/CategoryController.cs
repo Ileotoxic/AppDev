f890668beeccb5dea0096806bc0e84889bf654a6
Pham Thanh Son <sonpt28@fpt.edu.vn>
2024-04-05 14:12:24 +0700

    Create Area

---
+++ BookShop/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,92 @@
+﻿using BookShop.Data;
+using BookShop.Models;
+using BookShop.Repository;
+using BookShop.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookShop.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public CategoryController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            List<Category> myList = _unitOfWork.CategoryRepository.GetAll().ToList();
+            return View(myList);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Category category)
+        {
+            if (category.Name == category.Description)
+            {
+                ModelState.AddModelError("Description", "Description must be different than Name");
+            }
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.CategoryRepository.Add(category);
+                _unitOfWork.Save();
+                TempData["success"] = "Category created succesfully";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? category = _unitOfWork.CategoryRepository.Get(c => c.Id == id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.CategoryRepository.Update(category);
+                _unitOfWork.Save();
+                TempData["success"] = "Category updated succesfully";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? category = _unitOfWork.CategoryRepository.Get(c => c.Id == id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+        [HttpPost]
+        public IActionResult Delete(Category category)
+        {
+            _unitOfWork.CategoryRepository.Delete(category);
+            _unitOfWork.Save();
+            TempData["success"] = "Category deleted succesfully";
+            return RedirectToAction("Index");
+
+        }
+    }
+}
